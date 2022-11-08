d9e50b66f055baf6e2c96a8bac64b41b5a658858
user09 <user01@user.50>
2022-11-08 09:49:51 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        https://github.com/larisa12/nob08.git
+        }
+    }
+}
